b758b5660f7c994317c8089b766a86f94b48637c
badmuffin <itsabhijeet06@gmail.com>
2024-09-27 15:05:52 +0530

    Hero section I

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Hero from './component/Hero'
+import Navbar from './component/Navbar'
 
 function App() {
   
   return (
     <>
       <section>
+        <Navbar />
         <Hero />
       </section>
     </>
